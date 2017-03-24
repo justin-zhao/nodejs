7b44174460300ccfb0ea1a6dad6d8454707c212d
Justin Zhao <justin-zhao@qq.com>
2017-03-24 14:35:05 +0800

    Simplify the server.js
    Signed-off-by: Justin Zhao <justin-zhao@qq.com>

--- server.js
+++ server.js
@@ -4,40 +4,25 @@ var fs = require('fs');
 
 http.createServer(function (request, response) {
 	var pathname = url.parse(request.url).pathname;
-	//var pathname = "/canvas.h5";
-	//console.log("Request for " + pathname + " received. type="+response.headers["content-type"] + ", method=" + method + "\n");
-	//console.log("Request for " + pathname + " received. method=" + request.method + ", head=" + request.headers);
+	//console.log("Request for " + pathname + " received. method=" + request.method);
 	
-	fs.readFile(pathname.substr(1), function (err, data) {
-		if (err) {
-			console.log(err);
-			response.writeHead(404, {'Content-Type': 'text/html'});
-		}else if (pathname  == "/car.png") {
-//			console.log("write image");
-			response.writeHead(200, {'Content-Type': 'image/png'});	
-			response.write(data, "binary");
-		} else{	         
-			//console.log("write text");
-			response.writeHead(200, {'Content-Type': 'text/html'});	
-			response.write(data.toString());		
-		}
-
-		response.end();
-	});   
+	if (request.method == "GET")
+	{
+		fs.readFile(pathname.substr(1), function (err, data) {
+			if (err) {
+				console.log(err);
+				response.writeHead(404, {'Content-Type': 'text/html'});
+			}else if (pathname.substr(pathname.length-4) == ".png") {
+				response.writeHead(200, {'Content-Type': 'image/png'});	
+				response.write(data, "binary");
+			} else{	         
+				response.writeHead(200, {'Content-Type': 'text/html'});	
+				response.write(data.toString());		
+			}
+	
+			response.end();
+		});   
+	}
 }).listen(8080);
 
-/*var fs = require('fs');
-
-http.createServer(function (request, response) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	//response.writeHead(200, {'Content-Type': 'text/plain'});
-
-	var myData = fs.readFileSync('canvas.h5');
-//	response.end('Hello, World\n');
-	response.write(myData.toString());
-	response.end();
-}).listen(8080);
-*/
-
-//console.log(myData.toString());
 console.log('Server runing at http://127.0.0.1:8080/');
